c634636b2882db8c0c723f696d5b5989a5fd68f3
Gerro Den <GerroDen@users.noreply.github.com>
2024-12-20 17:45:14 +0100

    Set root class name as intended from dev-tools design system

--- src/panel/main.ts
+++ src/panel/main.ts
@@ -4,5 +4,4 @@ import "virtual:uno.css";
 import { createApp } from "vue";
 import VueJsonPretty from "vue-json-pretty";
 
-document.body.classList.toggle("theme-with-dark-background", document.body.classList.contains("dark"));
 createApp(Devtools).component("json-pretty", VueJsonPretty).mount("#app");
