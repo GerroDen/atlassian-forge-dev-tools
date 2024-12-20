ac75f53d66d26940fd494cf6b56b3002b1c7d316
Gerro Den <GerroDen@users.noreply.github.com>
2024-12-20 17:44:46 +0100

    Move styling into a single css file

--- src/panel/main.ts
+++ src/panel/main.ts
@@ -1,13 +1,8 @@
 import Devtools from "./Devtools.vue";
-import "chrome-devtools-frontend/front_end/ui/legacy/applicationColorTokens.css";
-import "chrome-devtools-frontend/front_end/ui/legacy/designTokens.css";
-import "chrome-devtools-frontend/front_end/ui/legacy/inspectorCommon.css";
-import "chrome-devtools-frontend/front_end/ui/legacy/themeColors.css";
-import "chrome-devtools-frontend/front_end/ui/legacy/tokens.css";
+import "./main.css";
 import "virtual:uno.css";
 import { createApp } from "vue";
 import VueJsonPretty from "vue-json-pretty";
-import "vue-json-pretty/lib/styles.css";
 
 document.body.classList.toggle("theme-with-dark-background", document.body.classList.contains("dark"));
 createApp(Devtools).component("json-pretty", VueJsonPretty).mount("#app");
